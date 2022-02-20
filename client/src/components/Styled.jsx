dfaf013c9e1c363d350f98a98477a0d46d60d2c5
Ratnakar <ratnakar3895@yahoo.com>
2022-02-20 15:59:19 +0530

    feat: front-end development initiated, added navbar

---
+++ client/src/components/Styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    padding: 5px 15px;
+    background-color: #f8b9d4;
+    border: none;
+    display: flex;
+    color: black;
+    text-transform: capitalize;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+    border-radius: 5px;
+
+    &:hover {
+        background-color: #f897c1;
+    }
+`;
