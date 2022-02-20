d5274c3ce037589db21c2c8458ca99df103a0c47
Ratnakar <ratnakar3895@yahoo.com>
2022-02-20 18:40:10 +0530

    feat: added cohorts and projects

---
+++ client/src/config/projects.js
@@ -0,0 +1,58 @@
+const projectList = [
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+    {
+        title: "project-oregano",
+        about: "The official repository of HackNITR",
+        techStack: "Gatsby, TailwindCSS, styled-componets",
+        link: "https://github.com/dscnitrourkela/project-oregano",
+    },
+];
+
+export default projectList;
