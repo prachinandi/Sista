77d95238c8e01537c8d7b2900d2ce670cf694296
Hemant Bajaj <41hemu38@gmail.com>
2022-02-20 18:07:10 +0530

    feat: integrated firebase auth and setup context api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Homepage from './components/Homepage';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 
-import Community from './components/views/Community';
-import AuthDetails from './components/views/AuthDetails';
+import Community from "./components/views/Community";
+import AuthDetails from "./components/views/AuthDetails";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/cohorts/team-asphalt" element={<Community />} />
           <Route exact path="/register/details" element={<AuthDetails />} />
+          <Route exact path="/register/signup" element={<SignUp />} />
         </Routes>
 
         <Footer />
@@ -24,11 +25,5 @@ function App() {
     </div>
   );
 }
-
-const Container = styled.div`
-  background-color: white;
-  min-height: 100vh;
-  max-width: 100vw;
-`;
 
 export default App;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import styled from "styled-components";
+import AuthContext from "../store/AuthContext";
 import {
   Button,
   Container,
@@ -14,6 +15,76 @@ import {
   UnderLine,
 } from "./Register";
 const SignUp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const profileCtx = useContext(AuthContext);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const userNameRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+    const enteredUserName = userNameRef.current.value;
+
+    let token;
+
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA9qngTQ2kQOuahg4Y6ZodqIBo-sTXXvDI";
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+          //....
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed";
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        token = data.idToken;
+        fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA9qngTQ2kQOuahg4Y6ZodqIBo-sTXXvDI",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              idToken: data.idToken,
+              displayName: enteredUserName,
+              photoUrl: "",
+              returnSecureToken: false,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            profileCtx.login(token, data.displayName);
+          });
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <Container>
       <SubContainer>
@@ -21,10 +92,10 @@ const SignUp = () => {
         <UnderLine style={{ margin: 0 }} />
         <PopupContainer>
           <PopupContainerLeft>
-            <AuthForm>
-              <AuthInput placeholder="userName" />
-              <AuthInput placeholder="email" />
-              <AuthInput placeholder="Password" />
+            <AuthForm onSubmit={submitHandler}>
+              <AuthInput ref={userNameRef} placeholder="userName" />
+              <AuthInput ref={emailRef} placeholder="email" />
+              <AuthInput ref={passwordRef} placeholder="Password" />
               <Button>Proceed Now -&gt;</Button>
               <LinkText>
                 Already registered? <a href="/register/login">Sign in here</a>
