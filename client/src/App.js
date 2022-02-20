a213517983074712fe6274fd5f4566e3dc7f7e2b
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-02-20 16:53:10 +0530

    feat:community page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
-import "./App.css";
-import styled from "styled-components";
-import Navbar from "./components/Navbar";
+import './App.css';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Community from './components/views/Community';
+
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar></Navbar>
-            <Container></Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/cohorts/team-asphalt" element={<Community />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 const Container = styled.div`
-    background-color: white;
-    min-height: 100vh;
-    max-width: 100vw;
+  background-color: white;
+  min-height: 100vh;
+  max-width: 100vw;
 `;
 
 export default App;
---
+++ client/src/components/Community/ProfileCards.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGithub, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const image = 'https://res.cloudinary.com/sambitsankalp/image/upload/v1645337230/stormhacks/Background_kjasm3.png';
+
+const ImageContainer = styled.div`
+  background-image: url(${image});
+  width: 400px;
+  height: 512px;
+  background-repeat: no-repeat;
+  display: flex;
+  margin: 25px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextName = styled.h2`
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Montserrat', 'Helvetica Neue', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  margin: 0px;
+  margin-top: 150px;
+  color: #000000;
+`;
+
+const Description = styled.p`
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Montserrat', 'Helvetica Neue', sans-serif;
+  font-style: normal;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 5px;
+  padding: 0px 30px;
+  color: #000000;
+`;
+
+
+const Titles = styled.p`
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Montserrat', 'Helvetica Neue', sans-serif;
+  font-style: normal;
+  font-size: 26px;
+  font-weight: 600;
+  text-align: center;
+  margin: 0px;
+  padding: 0px 30px;
+  color: #000000;
+`;
+
+const HorizontalLine = styled.div`
+  width: 10%;
+  height: 1px;
+  margin-top: 3px;
+  background-color: #000;
+  opacity: 1;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  .icon {
+    width: 25px;
+    height: 25px;
+    color: #000000;
+    cursor: pointer;
+    margin-right: 13px;
+    margin-top: 10px;
+    @media (max-width: 660px) {
+      width: 15px;
+      height: 15px;
+      margin-right: 5px;
+    }
+  }
+`;
+
+const links = [
+  {
+    link: 'https://www.linkedin.com/',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://www.instagram.com/',
+    icon: faInstagram,
+  },
+  {
+    link: 'https://www.instagram.com/',
+    icon: faFacebookF,
+  },
+  {
+    link: 'https://www.instagram.com/',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.instagram.com/',
+    icon: faTwitter,
+  },
+];
+
+const ProfileCard = () => (
+  <ImageContainer>
+    <TextName>Prachi Nandi</TextName>
+    <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut placerat nibh.</Description>
+    <Titles>Expertise</Titles>
+    <HorizontalLine />
+    <Description>Web Developer(MERN)</Description>
+    <Titles>Socials</Titles>
+    <HorizontalLine />
+    <LinkContainer>
+      {links.map(({ link, icon }) => (
+        <a key={link} href={link} target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon className='icon' icon={icon} />
+        </a>
+      ))}
+    </LinkContainer>
+  </ImageContainer>
+);
+
+export default ProfileCard;
